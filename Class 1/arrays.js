8f4b5ccd669cf0141df0fd1caeb6a5dfb617b3e1
tejasvi raj <tejasviraj8686@gmail.com>
2023-12-08 09:37:45 +0530

    added

--- Class 1/arrays.js
+++ Class 1/arrays.js
@@ -1,10 +1,5 @@
 //write a program print all the even number in an array.
 
-//$$$$$$$$$$$$ write a program to print the biggest number in an arrays.
-
-//write a program that prints all the male people's first name given a complex object.
-
-//write a program that reverse all the element of an array.
 
 // let person1 = "harkirat";
 // let person2 = "raman";
@@ -20,6 +15,14 @@ for (let i = 0; i < numberofpeople; i++) {
     console.log(ages[i]);
   }
 }
+
+//$$$$$$$$$$$$ write a program to print the biggest number in an arrays.
+
+
+
+//write a program that reverse all the element of an array.
+
+
 
 
 
