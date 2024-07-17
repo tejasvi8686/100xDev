aa93b0288ca04d66714b3816661cf09780d512f6
tejavi8686 <tejasviraj8686@gmail.com>
2024-07-17 21:00:47 +0530

    week 1

--- Week 1/main.js
+++ Week 1/main.js
@@ -1,16 +1,77 @@
-console.log("hello tejasvi");
-console.log(a);
+// let firstName = "tejasvi";
+// let age = 18;
+// let isMarried = "monkey";
 
 
+// if (isMarried == true) {
+//     console.log(firstName + " is married");
+// }
+
+// if (isMarried == false) {
+//     console.log(firstName + " is not married");
+// }
+
+// else {
+//     console.log(firstName + " is married");
+// }
+
+// console.log("this person name is "+ first + " and their age is" + age);
+
+// let answer = 0;
+
+// for (let i = 0; i <= 100; i = i + 1) {
+//   answer = answer + i;
+// }
+
+// console.log(answer);
+
+// for (let i = 1; i <= 100; i++){
+//     console.log(i);
+// }
+
+// write a program to great a person given thier first name and last name
+
+// function greet(firstName, lastName) {
+//   return firstName + " " + lastName;
+
+// }
+
+// console.log(greet("Tejasvi", "Raj"));
+
+// write a program that greets a person based on thier gender .
+
+// function greets(firstName, lastName, gender) {
+//   let fullName = firstName + " " + lastName;
+
+//   let greeting;
+
+//   // Check the gender and set the appropriate greeting
+//   if (gender === "male") {
+//     greeting = "Hello, Mr. " + fullName + "!";
+//   } else if (gender === "female") {
+//     greeting = "Hello, Ms. " + fullName + "!";
+//   } else {
+//     greeting = "Hello, " + fullName + "!";
+//   }
+
+//   console.log(greeting);
+// }
+
+// greets("Tejasvi", "Raj", "male");
+// greets("Priya", "Kumar", "female");
+// greets("Alex", "Taylor", "non-binary");
 
 
+//write a program print all the even number is an array
 
-//Let's write some code -
-
-//1.write the program to great a person thier first and last name
-
-//2. write a program that greets a person based on thier gender.(if else)
-
-//3. write a program that count from 0-1000 and prints (for loop)
+const ages = [21, 22, 23, 24, 25, 26, 100]
+const numberofPeople = ages.length;
 
 
+for (let i = 0; i< numberofPeople; i++){
+  if(ages[i] % 2 == 0){
+    
+    console.log(ages[i]);
+
+  }
+}
--- Week 1/varriable.js
+++
@@ -1,5 +0,0 @@
-//variables (let , var , const)
-
-const a = 1;
-a = 5;
-console.log(a);
