8ad89cf140bd72d323e0fff3162cf3b3864020d7
tejasvi8686 <tejasviraj8686@gmail.com>
2025-05-14 22:27:20 +0530

    Refactor greeting function and add sum calculation example

--- Week 1/problemsolving.js
+++ Week 1/problemsolving.js
@@ -10,7 +10,7 @@
 
 // function greetperson(firstName, lastName, gender) {
 //   if (gender === "male") {
-//     console.log("Hello " + firstName + " " + lastName);
+//     console.log("Hello " + firstName + " " + lastName + "  " + gender);
 //   } else if (gender === "female") {
 //     console.log("Hello " + firstName + " " + lastName);
 //   } else {
@@ -49,14 +49,13 @@
 // for (let i = 0; i < arr.length; i++) {
 //   if (arr[i] > biggest) {
 //     biggest = arr[i];
-   
-//   }    
+
+//   }
 // }
 
 // console.log(biggest);
 
-
-//write a program that print all the male  people  first name given  a complex object 
+//write a program that print all the male  people  first name given  a complex object
 
 // let people = [
 //   { firstName: "Tejasvi", lastName: "Raj", gender: "female" },
@@ -67,22 +66,38 @@
 //   { firstName: "raj", lastName: "tejasvi", gender: "male" },
 // ];
 
-
-
-
 // for (let i = 0; i < people.length; i++) {
 //   if (people[i].gender === "male") {
 //     console.log(people[i].firstName + " " + people[i].lastName);
 //   }
 // }
 
- // write a prgram that reverse all the elements of an array
- let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
- let reversed = [];
- console.log("reversed array is", reversed);
- 
- for (let i = arr.length - 1; i >= 0; i--) {
-   reversed.push(arr[i]);
- }
- console.log(reversed);
+// write a prgram that reverse all the elements of an array
+//  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//  let reversed = [];
+//  console.log("reversed array is", reversed);
 
+//  for (let i = arr.length - 1; i >= 0; i--) {
+//    reversed.push(arr[i]);
+//  }
+//  console.log(reversed);
+
+function sum(num1, num2, fntoCall) {
+  let result = num1 + num2;
+  fntoCall(result);
+}
+
+function displayResult(data) {
+  console.log("the sum of the two numbers is", data);
+}
+
+const ans = sum(10, 20, displayResult);
+console.log("ans", ans);
+
+
+function calculateAirthmatic (a,b, type){}
+
+function calculateAirthmatic(a, b, arithmeticFinalFunction) {
+  const ans =  arithmeticFinalFunction(a, b);
+  
+}
